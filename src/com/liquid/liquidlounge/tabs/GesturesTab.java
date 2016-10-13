6977040a37273264d579d040bc01664c29722c52
jpardogo <jpardogo@gmail.com>
2016-10-13 16:34:36 -0500

    LiquidLounge: Rework PagerSlidingTabStrip
    This is based from jpardogo's take on PagerSlidingTabStrip from here:
    https://github.com/jpardogo/PagerSlidingTabStrip
    
    So he gets authorship :D
    
    This gives it a slightly cleaner look and a more defined material look.
    Also, clean out more stuff from AndroidStudio
    
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

---
+++ src/com/liquid/liquidlounge/tabs/GesturesTab.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright (C) 2017 Screw'd AOSP
+ * Copyright (C) 2017 The Liquid Remix Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.liquid.liquidlounge.tabs;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
+import android.graphics.drawable.TransitionDrawable;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.widget.Toolbar;
+import android.support.v13.app.FragmentPagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import com.android.settings.R;
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.SettingsPreferenceFragment;
+
+import com.liquid.liquidlounge.PagerSlidingTabStrip;
+import com.android.settings.gestures.GestureSettings;
+import com.liquid.liquidlounge.fragments.GesturesSettings;
+import com.liquid.liquidlounge.fragments.CarbonGesturesSettings;
+
+public class GesturesTab extends SettingsPreferenceFragment {
+
+    ViewPager mViewPager;
+    ViewGroup mContainer;
+    PagerSlidingTabStrip mTabs;
+    SectionsPagerAdapter mSectionsPagerAdapter;
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mContainer = container;
+
+        View view = inflater.inflate(R.layout.gestures_tab, container, false);
+        mViewPager = (ViewPager) view.findViewById(R.id.gestures_viewpager);
+        mTabs = (PagerSlidingTabStrip) view.findViewById(R.id.gestures_tabs);
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getFragmentManager());
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+        mTabs.setViewPager(mViewPager);
+        setHasOptionsMenu(true);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle saveState) {
+        super.onSaveInstanceState(saveState);
+    }
+
+    class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        String titles[] = getTitles();
+        private Fragment frags[] = new Fragment[titles.length];
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+            frags[0] = new GesturesSettings();
+            frags[1] = new GestureSettings();
+            frags[2] = new CarbonGesturesSettings();
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return frags[position];
+        }
+
+        @Override
+        public int getCount() {
+            return frags.length;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles[position];
+        }
+    }
+
+    private String[] getTitles() {
+        String titleString[];
+        titleString = new String[]{
+                getString(R.string.gestures_title),
+                getString(R.string.gesture_preference),
+                getString(R.string.carbon_gesture_preference_title)};
+
+        return titleString;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.LIQUID;
+    }
+}
---
+++ src/com/liquid/liquidlounge/tabs/StatusBarTab.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright (C) 2017 Screw'd AOSP
+ * Copyright (C) 2017 The Liquid Remix Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.liquid.liquidlounge.tabs;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
+import android.graphics.drawable.TransitionDrawable;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.widget.Toolbar;
+import android.support.v13.app.FragmentPagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import com.android.settings.R;
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.SettingsPreferenceFragment;
+
+import com.liquid.liquidlounge.PagerSlidingTabStrip;
+import com.liquid.liquidlounge.fragments.StatusBarSettings;
+import com.liquid.liquidlounge.fragments.BatteryBarSettings;
+import com.liquid.liquidlounge.fragments.CarrierLabel;
+import com.liquid.liquidlounge.fragments.StatusBarClockSettings;
+
+public class StatusBarTab extends SettingsPreferenceFragment {
+
+    ViewPager mViewPager;
+    ViewGroup mContainer;
+    PagerSlidingTabStrip mTabs;
+    SectionsPagerAdapter mSectionsPagerAdapter;
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mContainer = container;
+
+        View view = inflater.inflate(R.layout.statusbar_tab, container, false);
+        mViewPager = (ViewPager) view.findViewById(R.id.statusbar_viewpager);
+        mTabs = (PagerSlidingTabStrip) view.findViewById(R.id.statusbar_tabs);
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getFragmentManager());
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+        mTabs.setViewPager(mViewPager);
+        setHasOptionsMenu(true);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle saveState) {
+        super.onSaveInstanceState(saveState);
+    }
+
+    class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        String titles[] = getTitles();
+        private Fragment frags[] = new Fragment[titles.length];
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+            frags[0] = new StatusBarSettings();
+            frags[1] = new BatteryBarSettings();
+            frags[2] = new CarrierLabel();
+            frags[3] = new StatusBarClockSettings();
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return frags[position];
+        }
+
+        @Override
+        public int getCount() {
+            return frags.length;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles[position];
+        }
+    }
+
+    private String[] getTitles() {
+        String titleString[];
+        titleString = new String[]{
+                getString(R.string.statusbar_title),
+                getString(R.string.battery_bar_title),
+                getString(R.string.carrier_label_title),
+                getString(R.string.statusbar_clock_title)};
+
+        return titleString;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.LIQUID;
+    }
+}
