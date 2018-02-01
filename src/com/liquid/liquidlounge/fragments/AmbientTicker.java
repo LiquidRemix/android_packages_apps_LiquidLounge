odroid-n2/android_packages_apps_RMSettings

Ambient music ticker: add a full disable mode [2/2]

src/com/abc/settings/AmbientTicker.java
@@ -1,0 +1,70 @@
+/*
+ * Copyright (C) 2017 The ABC rom
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.abc.settings;
+
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.provider.Settings;
+import android.support.v7.preference.Preference;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class AmbientTicker extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private ListPreference mAmbientTicker;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.abc_ambient_ticker);
+        final ContentResolver resolver = getActivity().getContentResolver();
+
+        mFooterPreferenceMixin.createFooterPreference().setTitle(R.string.ambient_ticker_footer);
+
+        mAmbientTicker = (ListPreference) findPreference("force_ambient_for_media");
+        int mode = Settings.System.getIntForUser(resolver,
+                Settings.System.FORCE_AMBIENT_FOR_MEDIA, 0, UserHandle.USER_CURRENT);
+        mAmbientTicker.setValue(Integer.toString(mode));
+        mAmbientTicker.setSummary(mAmbientTicker.getEntry());
+        mAmbientTicker.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.ABC;
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        final ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mAmbientTicker) {
+            int mode = Integer.valueOf((String) newValue);
+            int index = mAmbientTicker.findIndexOfValue((String) newValue);
+            mAmbientTicker.setSummary(
+                    mAmbientTicker.getEntries()[index]);
+            Settings.System.putIntForUser(resolver, Settings.System.FORCE_AMBIENT_FOR_MEDIA,
+                    mode, UserHandle.USER_CURRENT);
+            return true;
+        }
+        return false;
+    }
+}

LiquidRemix/android_packages_apps_LiquidLounge

src/com/liquid/liquidlounge/fragments/AmbientTicker.java
@@ -1,0 +1,74 @@
+/*
+ * Copyright (C) 2017 The Nitrogen Project
+ * Copyright (C) 2017 The Liquid Remix Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.liquid.liquidlounge.fragments;
+
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.provider.Settings;
+import android.support.v7.preference.Preference;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class AmbientTicker extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private ListPreference mAmbientTicker;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.ambient_ticker);
+
+        final ContentResolver resolver = getActivity().getContentResolver();
+
+        mFooterPreferenceMixin.createFooterPreference().setTitle(R.string.ambient_ticker_footer);
+
+        mAmbientTicker = (ListPreference) findPreference("force_ambient_for_media");
+        int mode = Settings.System.getIntForUser(resolver,
+                Settings.System.FORCE_AMBIENT_FOR_MEDIA, 0, UserHandle.USER_CURRENT);
+        mAmbientTicker.setValue(Integer.toString(mode));
+        mAmbientTicker.setSummary(mAmbientTicker.getEntry());
+        mAmbientTicker.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.LIQUID;
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        final ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mAmbientTicker) {
+            int mode = Integer.valueOf((String) newValue);
+            int index = mAmbientTicker.findIndexOfValue((String) newValue);
+            mAmbientTicker.setSummary(
+                    mAmbientTicker.getEntries()[index]);
+            Settings.System.putIntForUser(resolver, Settings.System.FORCE_AMBIENT_FOR_MEDIA,
+                    mode, UserHandle.USER_CURRENT);
+            return true;
+        }
+        return false;
+    }
+}