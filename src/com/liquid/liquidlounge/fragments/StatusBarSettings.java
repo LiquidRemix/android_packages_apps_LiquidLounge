7ec0d7b510a182f7b9050c94f6634737217701a3
ezio84 <brabus84@gmail.com>
2018-02-12 22:44:27 +0100

    Move sysui tuner percentage settings to custom battery settings [2/2]
    * Hide landscape battery mode for now

--- src/com/liquid/liquidlounge/fragments/StatusBarSettings.java
+++ src/com/liquid/liquidlounge/fragments/StatusBarSettings.java
@@ -60,6 +60,7 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
     private SystemSettingSwitchPreference mNetMonitor;
     private ListPreference mTickerMode;
     private ListPreference mBatteryIconStyle;
+    private SwitchPreference mBatteryPercentage;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -91,10 +92,19 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
         mTickerMode.setValue(String.valueOf(tickerMode));
         mTickerMode.setSummary(mTickerMode.getEntry());
 
+        int batteryStyle = Settings.Secure.getInt(resolver,
+                Settings.Secure.STATUS_BAR_BATTERY_STYLE, 0);
         mBatteryIconStyle = (ListPreference) findPreference(BATTERY_STYLE);
-        mBatteryIconStyle.setValue(Integer.toString(Settings.Secure.getInt(resolver,
-                Settings.Secure.STATUS_BAR_BATTERY_STYLE, 0)));
+        mBatteryIconStyle.setValue(Integer.toString(batteryStyle));
         mBatteryIconStyle.setOnPreferenceChangeListener(this);
+
+        boolean show = Settings.System.getInt(resolver,
+                Settings.System.SHOW_BATTERY_PERCENT, 1) == 1;
+        mBatteryPercentage = (SwitchPreference) findPreference("show_battery_percent");
+        mBatteryPercentage.setChecked(show);
+        mBatteryPercentage.setOnPreferenceChangeListener(this);
+        boolean hideForcePercentage = batteryStyle == 6 || batteryStyle == 7; /*text or hidden style*/
+        mBatteryPercentage.setEnabled(!hideForcePercentage);
     }
 
     @Override
@@ -125,6 +135,14 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
             int value = Integer.valueOf((String) objValue);
             Settings.Secure.putInt(getContentResolver(),
                     Settings.Secure.STATUS_BAR_BATTERY_STYLE, value);
+            boolean hideForcePercentage = value == 6 || value == 7;/*text or hidden style*/
+            mBatteryPercentage.setEnabled(!hideForcePercentage);
+            return true;
+        } else  if (preference == mBatteryPercentage) {
+            boolean value = (Boolean) objValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.SHOW_BATTERY_PERCENT, value ? 1 : 0);
+            mBatteryPercentage.setChecked(value);
             return true;
         }
         return false;
