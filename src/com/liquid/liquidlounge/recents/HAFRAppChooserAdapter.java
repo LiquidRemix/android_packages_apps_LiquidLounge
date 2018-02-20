d9b86edbeb675ffa88592769d55f05c3d9c09058
Christian Oder <myself5@carbonrom.org>
2018-02-20 19:28:57 +0100

    HAFRDialog: Add ability to only show Launcher Apps
    Change-Id: I79499be92fb75ecf85357cccbf9193971789459a
    
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- src/com/liquid/liquidlounge/recents/HAFRAppChooserAdapter.java
+++ src/com/liquid/liquidlounge/recents/HAFRAppChooserAdapter.java
@@ -52,6 +52,7 @@ public abstract class HAFRAppChooserAdapter extends BaseAdapter implements Filte
     protected List<PackageInfo> mTemporarylist;
 
     boolean isUpdating;
+    boolean hasLauncherFilter = false;
 
     public HAFRAppChooserAdapter(Context context) {
         mContext = context;
@@ -77,10 +78,14 @@ public abstract class HAFRAppChooserAdapter extends BaseAdapter implements Filte
                     item.icon = info.applicationInfo.loadIcon(mPackageManager);
                     item.packageName = info.packageName;
                     final int index = Collections.binarySearch(temp, item);
-                    if (index < 0) {
-                        temp.add((-index - 1), item);
-                    } else {
-                        temp.add((index + 1), item);
+                    final boolean isLauncherApp =
+                            mPackageManager.getLaunchIntentForPackage(info.packageName) != null;
+                    if (!hasLauncherFilter || isLauncherApp) {
+                        if (index < 0) {
+                            temp.add((-index - 1), item);
+                        } else {
+                            temp.add((index + 1), item);
+                        }
                     }
                 }
                 mHandler.post(new Runnable() {
@@ -195,5 +200,9 @@ public abstract class HAFRAppChooserAdapter extends BaseAdapter implements Filte
         TextView name;
         ImageView icon;
         TextView pkg;
+    }
+
+    protected void setLauncherFilter(boolean enabled) {
+        hasLauncherFilter = enabled;
     }
 }
