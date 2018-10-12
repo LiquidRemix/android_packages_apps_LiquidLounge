7420fa64b75c948bb5782c0f23919e41fef72966
liquid0624 <rjbirnesser@gmail.com>
2018-10-11 21:03:51 -0400

    lounge: cleanup gesture settings
    Signed-off-by: liquid0624 <rjbirnesser@gmail.com>

--- src/com/liquid/liquidlounge/fragments/GestureSettings.java
+++ src/com/liquid/liquidlounge/fragments/GestureSettings.java
@@ -22,16 +22,16 @@ import android.support.v7.preference.Preference;
 import android.support.v7.preference.ListPreference;
 
 import com.android.settings.R;
-import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.SettingsPreferenceFragment;
 
-public class GestureSettings extends SettingsPreferenceFragment implements
-                                Preference.OnPreferenceChangeListener {
+public class GestureSettings extends SettingsPreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
 
-     private static final String KEY_TORCH_LONG_PRESS_POWER_TIMEOUT =
+    private static final String KEY_TORCH_LONG_PRESS_POWER_TIMEOUT =
             "torch_long_press_power_timeout";
 
-     private ListPreference mTorchLongPressPowerTimeout;
+    private ListPreference mTorchLongPressPowerTimeout;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -42,7 +42,7 @@ public class GestureSettings extends SettingsPreferenceFragment implements
         mTorchLongPressPowerTimeout =
                     (ListPreference) findPreference(KEY_TORCH_LONG_PRESS_POWER_TIMEOUT);
 
-         mTorchLongPressPowerTimeout.setOnPreferenceChangeListener(this);
+        mTorchLongPressPowerTimeout.setOnPreferenceChangeListener(this);
         int TorchTimeout = Settings.System.getInt(getContentResolver(),
                         Settings.System.TORCH_LONG_PRESS_POWER_TIMEOUT, 0);
         mTorchLongPressPowerTimeout.setValue(Integer.toString(TorchTimeout));
@@ -50,7 +50,7 @@ public class GestureSettings extends SettingsPreferenceFragment implements
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-         if (preference == mTorchLongPressPowerTimeout) {
+        if (preference == mTorchLongPressPowerTimeout) {
             String TorchTimeout = (String) newValue;
             int TorchTimeoutValue = Integer.parseInt(TorchTimeout);
             Settings.System.putInt(getActivity().getContentResolver(),
