560e25c8d542223c4fbcd807bdc1b39047efb5a2
maxwen <max.weninger@gmail.com>
2018-12-17 00:11:48 +0530

    [2/2] Lounge: op gesture tweaks
    Change-Id: Ia51487c1978613ef7b4410e33795adbf0f55801f

--- src/com/liquid/liquidlounge/fragments/OmniGestureSettings.java
+++ src/com/liquid/liquidlounge/fragments/OmniGestureSettings.java
@@ -17,36 +17,85 @@
 
 package com.liquid.liquidlounge.fragments;
 
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.RemoteException;
+import android.os.UserHandle;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
+import android.provider.Settings;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.WindowManagerGlobal;
+
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.liquid.liquidlounge.preferences.SystemSettingSeekBarPreference;
 
-import android.content.ContentResolver;
-import android.content.Context;
-import android.content.res.Resources;
-import android.os.Bundle;
-import android.support.v7.preference.Preference;
-import android.support.v7.preference.PreferenceScreen;
-import android.provider.Settings;
+import com.android.internal.logging.nano.MetricsProto;
 
-import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+public class OmniGestureSettings extends SettingsPreferenceFragment implements
+         OnPreferenceChangeListener {
 
-public class OmniGestureSettings extends SettingsPreferenceFragment {
-    private static final String TAG = "OmniGestureSettings";
+    private static final String KEY_SWIPE_LENGTH = "gesture_swipe_length";
+    private static final String KEY_SWIPE_TIMEOUT = "gesture_swipe_timeout";
+
+    private SystemSettingSeekBarPreference mSwipeTriggerLength;
+    private SystemSettingSeekBarPreference mSwipeTriggerTimeout;
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        addPreferencesFromResource(R.xml.omni_gesture_settings);
+
+        PreferenceScreen prefSet = getPreferenceScreen();
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        // use bottom gestures
+        mSwipeTriggerLength = (SystemSettingSeekBarPreference) findPreference(KEY_SWIPE_LENGTH);
+        int triggerLength = Settings.System.getInt(resolver, Settings.System.BOTTOM_GESTURE_SWIPE_LIMIT,
+                getSwipeLengthInPixel(getResources().getInteger(com.android.internal.R.integer.nav_gesture_swipe_min_length)));
+        mSwipeTriggerLength.setValue(triggerLength);
+        mSwipeTriggerLength.setOnPreferenceChangeListener(this);
+
+        mSwipeTriggerTimeout = (SystemSettingSeekBarPreference) findPreference(KEY_SWIPE_TIMEOUT);
+        int triggerTimeout = Settings.System.getInt(resolver, Settings.System.BOTTOM_GESTURE_TRIGGER_TIMEOUT,
+                getResources().getInteger(com.android.internal.R.integer.nav_gesture_swipe_timout));
+        mSwipeTriggerTimeout.setValue(triggerTimeout);
+        mSwipeTriggerTimeout.setOnPreferenceChangeListener(this);
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mSwipeTriggerLength) {
+            int value = (Integer) newValue;
+            Settings.System.putInt(resolver,
+                    Settings.System.BOTTOM_GESTURE_SWIPE_LIMIT, value);
+            return true;
+        } else if (preference == mSwipeTriggerTimeout) {
+            int value = (Integer) newValue;
+            Settings.System.putInt(resolver,
+                    Settings.System.BOTTOM_GESTURE_TRIGGER_TIMEOUT, value);
+            return true;
+        }
+        return false;
+    }
+
+    private int getSwipeLengthInPixel(int value) {
+        return Math.round(value * getResources().getDisplayMetrics().density);
+    }
 
     @Override
     public int getMetricsCategory() {
-        return MetricsEvent.LIQUID;
-    }
-
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.omni_gesture_settings);
-        mFooterPreferenceMixin.createFooterPreference().setTitle(R.string.gesture_settings_info);
-    }
-
-    @Override
-    public boolean onPreferenceTreeClick(Preference preference) {
-        return super.onPreferenceTreeClick(preference);
+        return MetricsProto.MetricsEvent.LIQUID;
     }
 }
-
